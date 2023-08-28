26839f63411e95909f2c06ca8ce56b89eba5a15f
Siddhant Raj <siddhantraj@Siddhants-MacBook-Air.local>
2023-08-28 22:41:43 +0530

    add assests and change app.js

--- frontEnd/src/App.js
+++ frontEnd/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./assets/logo.jpg";
+import image2 from "./assets/image 2.png";
+
+import image3 from "./assets/image 3.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <nav>
+          <img src={logo} alt="logo" height="80px" width="80px" />
+          <h1>Complaint System</h1>
+          <div class="nav">
+            <a href="/index" class="btn">
+              Home
+            </a>
+          </div>
+        </nav>
+        <h3><center>Login</center></h3>
+    <div class="login_container">
+        <div class="user">
+            <img src={image3} alt="user login"/>
+            <a href="/userlogin" class="btn1">User Login</a>
+        </div>
+        <div class="user">
+            <img src={image2} alt="admin login"/>
+            <a href="/adminlogin" class="btn1">Admin Login</a>
+        </div>
+    </div>
+      
     </div>
   );
 }
