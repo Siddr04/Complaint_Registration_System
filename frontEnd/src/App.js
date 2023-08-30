ddc6d5e78016f7373d91c3156bce20cf2b391197
Siddhant Raj <siddhantraj@Siddhants-MacBook-Air.local>
2023-08-30 12:12:48 +0530

    Add home page

--- frontEnd/src/App.js
+++ frontEnd/src/App.js
@@ -1,4 +1,6 @@
 import logo from "./assets/logo.jpg";
+import image1 from "./assets/image 1.png";
+
 import image2 from "./assets/image 2.png";
 
 import image3 from "./assets/image 3.png";
@@ -8,26 +10,30 @@ function App() {
   return (
     <div className="App">
         <nav>
-          <img src={logo} alt="logo" height="80px" width="80px" />
-          <h1>Complaint System</h1>
-          <div class="nav">
-            <a href="/index" class="btn">
-              Home
-            </a>
-          </div>
-        </nav>
-        <h3><center>Login</center></h3>
-    <div class="login_container">
-        <div class="user">
-            <img src={image3} alt="user login"/>
-            <a href="/userlogin" class="btn1">User Login</a>
+        <img src={logo} alt="logo" height="80px" width="80px"/>
+        <h1>Complaint System</h1>
+        <div class="nav">
+            <a href="/getstarted" class="btn">Get Started</a>
+            <a href="/aboutus" class="btn">About us</a>
         </div>
-        <div class="user">
-            <img src={image2} alt="admin login"/>
-            <a href="/adminlogin" class="btn1">Admin Login</a>
+        
+    </nav>
+    <div class="container">
+        <div class="center">
+            <h1>Handling customer complaints is easier now.</h1>
+            <img src={image1} alt="Customer_image"/>
         </div>
+        
     </div>
-      
+    <div class="container2">
+        <h2>What is Complaint Management System?</h2>
+        <p>Complaint management is the process of handling, managing, responding to, and reporting customer grievances. This process requires a streamlined approach and constant monitoring to ensure faster resolutions.</p>
+
+    </div>
+    <div class="container3">
+        <h1>Save time, manage effort, and optimize productivity using intuitive Automations</h1>
+
+    </div>
     </div>
   );
 }
